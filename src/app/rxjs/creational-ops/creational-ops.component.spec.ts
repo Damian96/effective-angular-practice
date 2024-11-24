10ab509e381486f3cd7a8f0c30bf873ffabb92fa
Damianos Giankakis <damgiankakis@gmail.com>
2024-11-24 13:40:18 +0200

    Continues with merge, combineLatest, forkjoin

--- src/app/rxjs/creational-ops/creational-ops.component.spec.ts
+++ src/app/rxjs/creational-ops/creational-ops.component.spec.ts
@@ -28,7 +28,6 @@ describe('CreationalOpsComponent', () => {
   it('should log the concatenated observable', (done) => {
     // Arrange
     const expected = [1, 2, 4]; // Match actual emissions
-
     const receivedValues: any[] = [];
 
     // Act
@@ -46,10 +45,103 @@ describe('CreationalOpsComponent', () => {
     });
 
     // Trigger the observable to start emitting values
-    component.startConcat(); // This is the Act step, moving it to after the subscription
+    component.startConcat();
   });
 
+  it('should initialize the merge observable', () => {
+    // Arrange
 
+    // Act
 
+    // Assert
+    expect(component.merge).toBeTruthy();
+  })
+
+  it('should log the merged observable', (done) => {
+    // Arrange
+    const expected = ['B:1', 'A:1', 'A:2', 'B:2']; // Match actual emissions
+    const receivedValues: any[] = [];
+
+    // Act
+    component.merge$.subscribe({
+      next: (value) => {
+        receivedValues.push(value); // Capture emitted values
+      },
+      complete: () => {
+        console.info(receivedValues);
+
+        // Assert
+        expect(receivedValues).toEqual(expected); // Assert emissions directly
+        done(); // Mark test as completed
+      }
+    });
+
+    // Trigger the observable to start emitting values
+    component.startMerge();
+  });
+
+  it('should initialize the combineLatest observable', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    expect(component.combineLatest).toBeTruthy();
+  })
+
+  it('should log the combineLatest observable', (done) => {
+    // Arrange
+    const expected = [[1, 2], [10, 2]]; // Match actual emissions
+    const receivedValues: any[] = [];
+
+    // Act
+    component.combineLatest$.subscribe({
+      next: (value) => {
+        receivedValues.push(value); // Capture emitted values
+      },
+      complete: () => {
+        console.info(receivedValues);
+
+        // Assert
+        expect(receivedValues).toEqual(expected); // Assert emissions directly
+        done(); // Mark test as completed
+      }
+    });
+
+    // Trigger the observable to start emitting values
+    component.startCombine();
+  });
+
+  it('should initialize the forkjoin observable', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    expect(component.forkJoin).toBeTruthy();
+  })
+
+  it('should log the forkjoin observable', (done) => {
+    // Arrange
+    const expected = [1, 2, 3, 4]; // Match actual emissions
+    const receivedValues: any[] = [];
+
+    // Act
+    component.forkJoin$.subscribe({
+      next: (value) => {
+        receivedValues.push(value); // Capture emitted values
+      },
+      complete: () => {
+        console.info(receivedValues);
+
+        // Assert
+        expect(receivedValues).toEqual(expected); // Assert emissions directly
+        done(); // Mark test as completed
+      }
+    })
+
+    // Trigger the observable to start emitting values
+    component.startForkJoin();
+  })
 
 });
--- src/app/rxjs/creational-ops/creational-ops.component.ts
+++ src/app/rxjs/creational-ops/creational-ops.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { concat, merge, Subject } from "rxjs";
+import { combineLatest, concat, merge, Subject } from "rxjs";
 
 @Component({
   selector: 'app-creational-ops',
@@ -12,6 +12,15 @@ import { concat, merge, Subject } from "rxjs";
 export class CreationalOpsComponent implements OnInit {
 
   protected readonly destroy = inject(DestroyRef);
+
+  combineLatest = {
+    a: new Subject(),
+    b: new Subject()
+  }
+  combineLatest$ = combineLatest([
+    this.combineLatest.a,
+    this.combineLatest.b
+  ]).pipe(takeUntilDestroyed(this.destroy));
 
   concat = {
     a: new Subject(),
@@ -31,9 +40,16 @@ export class CreationalOpsComponent implements OnInit {
     this.merge.b
   ).pipe(takeUntilDestroyed(this.destroy));
 
-  ngOnInit() {
-    // this.concat$.subscribe(console.log);
+  forkJoin = {
+    a: new Subject(),
+    b: new Subject()
   }
+  forkJoin$ = merge(
+    this.forkJoin.a,
+    this.forkJoin.b
+  ).pipe(takeUntilDestroyed(this.destroy));
+
+  ngOnInit() { }
 
   startConcat() {
     this.concat.a.next(1);
@@ -45,5 +61,42 @@ export class CreationalOpsComponent implements OnInit {
     this.concat.b.complete();
 
     // Logs: B:1, A:1, A:2, B:2
+  }
+
+  startMerge() {
+    this.merge.b.next('B:1');
+    this.merge.a.next('A:1');
+    this.merge.a.next('A:2');
+    this.merge.b.next('B:2');
+
+    this.merge.a.complete();
+    this.merge.b.complete();
+    // Logs: B:1, A:1, A:2, B:2
+  }
+
+  startCombine() {
+    this.combineLatest.a.next(1);
+
+    setTimeout(() => { this.combineLatest.b.next(2), 100 });
+    setTimeout(() => { this.combineLatest.a.next(10), 500 });
+
+    setTimeout(() => {
+      this.combineLatest.a.complete();
+      this.combineLatest.b.complete();
+    }, 200)
+
+    // Logs: 1, 2, 10, 2
+  }
+
+  startForkJoin() {
+    this.forkJoin.a.next(1);
+    this.forkJoin.b.next(2);
+    this.forkJoin.a.next(3);
+    this.forkJoin.b.next(4);
+
+    this.forkJoin.a.complete();
+    this.forkJoin.b.complete();
+
+    // Logs: 1, 2, 3, 4
   }
 }
